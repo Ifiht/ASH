4d89f3dd793bc9fbe2baadce24c0f5471173a071
Peter Pan <peter@never.lan>
2022-01-04 19:09:19 +0000

    updated shell to posh

--- env/last.zsh
+++ env/last.zsh
@@ -1,3 +1,4 @@
+eval "$(oh-my-posh --init --shell zsh --config ~/.ash/configs/stelbent.minimal.omp.json)"
 # This line MUST be SECOND-LAST, readonly vars MUST be LAST
 source $HOME/.ash/submodules/zsh-syntax/zsh-syntax-highlighting.zsh 
 [ -n "$STY" ] && readonly TMOUT 
