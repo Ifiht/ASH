256b0887ff9def63332968dcd2f9feddd5db92c8
Peter Pan <peter@never.lan>
2022-01-04 20:45:25 +0000

    update init.sh

--- init.sh
+++ init.sh
@@ -31,12 +31,24 @@ case "$os" in
 	exit 1
 esac
 
+GPGCMD1=`which gpg`
+GPGCMD2=`which gpg2`
+
+if ! [ -z "$GPGCMD1" ]; then 
+    GPGCMD="$GPGCMD1"
+fi
+if ! [ -z "$GPGCMD2" ]; then 
+    GPGCMD="$GPGCMD2"
+fi
 
 #RVM installation:
 if ! [ -d $HOME/.rvm ]; then
 	echo "Installing RVM..."
-	#gpg --keyserver hkp://pool.sks-keyservers.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
-	gpg2 --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+    #$GPGCMD --keyserver hkp://pool.sks-keyservers.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+    #$GPGCMD --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+    #$GPGCMD --keyserver hkp://pgp.mit.edu --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+    #$GPGCMD --keyserver hkp://ipv4.pool.sks-keyservers.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+    #$GPGCMD --keyserver hkp://keyserver.pgp.com --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 	command curl -sSL https://rvm.io/mpapis.asc | gpg --import -
 	command curl -sSL https://rvm.io/pkuczynski.asc | gpg --import -
 	curl -sSL https://get.rvm.io | bash -s stable && . $HOME/.rvm/scripts/rvm
